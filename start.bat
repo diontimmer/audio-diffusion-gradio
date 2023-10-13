18c39dc9b60820c331884f2ab44fb0d3df62b63f
Dion Timmer <diontimmer@live.nl>
2023-10-13 09:26:04 -0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-"C:\Users\diont\mambaforge\envs\harmonai-tools\python.exe" "D:\DevWkspc\harmonai_gradio\gradio_extended.py"
+"C:\Users\diont\mambaforge\envs\harmonai-tools\python.exe" "D:\DevWkspc\audio-diffusion-gradio\gradio_extended.py"
